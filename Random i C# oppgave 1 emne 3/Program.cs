d8e50ec107950626d0bd60ead9b131477b73e527
KristianHeggdal <kristianheggdal@hotmail.no>
2024-11-19 16:26:19 +0100

    Add project files.

---
+++ Random i C# oppgave 1 emne 3/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+string[] hobbies = { "Dancing", "Cow-Tipping","Gambling","Glue sniffing" };
+Console.WriteLine("Who would like a new hobby?");
+string name = Console.ReadLine();
+string newHobby = Gethobby();
+Console.WriteLine($"{name} is now into: {newHobby}");
+string Gethobby()
+{
+    Random rand = new Random();
+    var randomNumber = rand.Next(0,hobbies.Length);
+    return hobbies[(randomNumber)];
+}
+
+;
